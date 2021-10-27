1a437056fbdfafc74ef9c373950a97fce0011f50
G984552019 <71371051+G984552019@users.noreply.github.com>
2021-10-27 18:09:17 +0900

    Add files via upload

---
+++ SnakeGame/js/keyPush.js
@@ -0,0 +1,18 @@
+function keyPush(env) {
+  switch (env.keyCode) {
+    case 37: xd = -1; yd = 0; break;
+    case 38: xd = 0; yd = -1; break;
+    case 39: xd = 1; yd = 0; break;
+    case 40: xd = 0; yd = 1; break;
+    case 32: stop(); break;
+  }
+}
+var move = false;
+function stop() {
+  if (move) {
+    clearInterval(timer);
+    move = false;
+  }else {
+    Snake();
+  }
+}
---
+++ SnakeGame/js/main.js
@@ -0,0 +1,35 @@
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canv.width, canv.height);
+ctx.fillStyle = "lime";
+ctx.fillRect(canv.width/2, canv.height/2, 20, 20);
+ctx.fillStyle = "red";
+ctx.fillRect(appleX*SIZE, appleY*SIZE, SIZE-2, SIZE-2);
+var timer;
+function Snake() {
+  move = true;
+  timer = setInterval(function(){
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canv.width, canv.height);
+    ctx.fillStyle = "lime";
+    px += xd;
+    py += yd;
+    snake.push({x:px, y:py});
+    for (let i=0; i<snake.length-1; i++) {
+      ctx.fillRect(snake[i].x*SIZE,snake[i].y*SIZE,SIZE-2,SIZE-2);
+      if (snake[i].x == px && snake[i].y == py) {
+        tail = MIN;
+      }
+    }
+    while (snake.length > tail) {
+      snake.shift();
+    }
+    if (appleX == px && appleY == py) {
+      tail++;
+      appleX = Math.floor(Math.random()*canv.width/SIZE);
+      appleY = Math.floor(Math.random()*canv.height/SIZE);
+    }
+    ctx.fillStyle = "red";
+    ctx.fillRect(appleX*SIZE, appleY*SIZE, SIZE-2, SIZE-2);
+  }, 1000/FPS);
+
+}
